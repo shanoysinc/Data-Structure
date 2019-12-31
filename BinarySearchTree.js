ba54358e269987e2469e0d5fed5bae6ea8b36c19
shanoysinc <shanoy96@gmail.com>
2019-12-31 11:46:45 -0500

    Added breadth first search

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -79,6 +79,23 @@ class BinarySearchTree {
             return false
         }
     }
+
+    // breadth first search
+    BFS() {
+        let data = [];
+        let queue = [];
+        let node = this.root
+
+        queue.push(node)
+        while (queue.length) {
+            node = queue.shift()
+            data.push(node)
+
+            if (node.left) queue.push(node.left)
+            if (node.right) queue.push(node.right)
+        }
+        return data
+    }
 }
 
 
