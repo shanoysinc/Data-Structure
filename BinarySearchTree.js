78f35e5b35979295988c81c30b92ae6ebe095157
shanoysinc <shanoy96@gmail.com>
2019-12-31 12:23:33 -0500

    rename methods

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -90,7 +90,6 @@ class BinarySearchTree {
         while (queue.length) {
             node = queue.shift()
             data.push(node)
-
             if (node.left) queue.push(node.left)
             if (node.right) queue.push(node.right)
         }
@@ -111,7 +110,7 @@ class BinarySearchTree {
         return data;
     }
 
-    sort() {
+    DFSInOrder() {
         let data = [];
         function traverse(node) {
 
@@ -124,7 +123,7 @@ class BinarySearchTree {
         return data;
     }
 
-    reverse() {
+    DFSInReverse() {
         let data = [];
         function traverse(node) {
 
@@ -140,9 +139,3 @@ class BinarySearchTree {
 
 
 const tree = new BinarySearchTree()
-tree.insert(10)
-tree.insert(6)
-tree.insert(15)
-tree.insert(3)
-tree.insert(8)
-tree.insert(20)
