dbc5dde8780784a29558b079e8fdbfb42054a981
shanoysinc <shanoy96@gmail.com>
2019-12-31 12:15:42 -0500

    added sort and reverse method

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -96,7 +96,25 @@ class BinarySearchTree {
         }
         return data
     }
+
+    // Depth first search  pre order
+    DFSPreOrder() {
+        let data = [];
+        function traverse(node) {
+            data.push(node.value);
+            if (node.left) traverse(node.left)
+            if (node.right) traverse(node.right)
+        }
+        traverse(this.root)
+        return data;
+    }
 }
 
 
 const tree = new BinarySearchTree()
+tree.insert(10)
+tree.insert(6)
+tree.insert(15)
+tree.insert(3)
+tree.insert(8)
+tree.insert(20)
